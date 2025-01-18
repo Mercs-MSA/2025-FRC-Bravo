3c39d096a6e4c02885dce66aff8a65f585e01c54
JathinBMercs <jathin.b@mckinneysteamacademy.org>
2025-01-17 20:53:05 -0600

    Added Commands for Scoring Stages

---
+++ src/main/java/frc/robot/commands/CommandChangeScoreStage.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+
+public class CommandChangeScoreStage extends Command {
+    public CommandChangeScoreStage(Constants.ScoringStageVal stage)
+    {
+        Constants.ScoringConstants.scoringStage = stage;
+    }
+    
+    @Override 
+    public void initialize() {
+        // This is where you put stuff that happens right at the start of the command
+    }
+
+    @Override 
+    public void execute() {
+        // This is where you put stuff that happens while the command is happening, it will loop here over and over
+    }
+
+    @Override 
+    public void end(boolean interrupted) {
+        // This is where you put stuff that happens when the command ends
+    }
+
+    @Override 
+    public boolean isFinished() {
+        
+        // This is where you put a statment that will determine wether a boolean is true or false
+        // This is checked after an execute loop and if the return comes out true the execute loop will stop and end will happen
+        // In this example, it will just instantly come out as true and stop the command as soon as it's called.
+        // System.out.println("isf");
+        // System.out.println(Constants.isWithinTol(pos, m_testIntakePivot.GetPosition(), Constants.TestIntakePivotConstants.tol));
+        // return Constants.isWithinTol(pos, m_testIntakePivot.GetPosition(), Constants.TestIntakePivotConstants.tol);
+        return true;
+    }
+
+}
