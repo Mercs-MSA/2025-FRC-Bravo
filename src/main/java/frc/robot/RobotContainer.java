6ead2a806dc7f371afac93cf7a34392414bd0206
MercenariesBrady <brady.h@mckinneysteamacademy.org>
2025-01-17 20:22:39 -0600

    test commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -70,7 +70,8 @@ public class RobotContainer {
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
         joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));   
+
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
@@ -83,7 +84,9 @@ public class RobotContainer {
 
 
         joystick.rightBumper().onTrue(new CommandClimb(Constants.ClimberConstants.positionUp));
+
         joystick.leftBumper().onTrue(new CommandClimb(Constants.ClimberConstants.positionDown));
+
         joystick.rightTrigger().onTrue(new CommandClimb(Constants.ClimberConstants.positionHold));
 
 
